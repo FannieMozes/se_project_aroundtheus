cf4ceffd4fa9bf42803d4ab98fd1f6653333a28c
Fannie Mozes <fanniejmozes@gmail.com>
2024-10-21 22:33:07 +0300

    Project 4 stage 3 - Index.JS has been updated with code that opens and closes the modal box by using the addEventListener() method, IDs have been added to HTML code for the buttons

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,15 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseModal = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileCloseModal.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
