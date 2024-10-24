5e70e6d39a1d98d403fb5a28d3ad2d74d12aaa02
Fannie Mozes <fanniejmozes@gmail.com>
2024-10-24 17:36:50 +0300

    Final stage of Project 4 - Around the U.S. - Added functionality to pre-fill the Edit Profile modal with existing values and discard unsaved changes when closed. Updated the profile fields on form submission without refreshing the page. Replaced static card elements with a template and created a getCardElement() function to dynamically render cards from an array + minor changes to the modals css styling.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project 3: Around The U.S.
+# Project 4: Around The U.S.
 
 ### Overview
 
@@ -22,9 +22,10 @@ Good luck and have fun!
 
 # Around the U.S
 
-This is the third project of the Software Engineering program at TripleTen.
+This is the fourth project of the Software Engineering program at TripleTen.
 
-- The project was created using HTML and CSS, based on the Figma design (https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
+- The project was created using HTML & CSS based on the Figma design (https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1).
+- JavaScript was integrated to manage user interactions, enabling users to edit their profile and dynamically render a gallery of cards based on data from an array.
 
 ## Project Description
 
@@ -51,6 +52,10 @@ Gallery Section: A dynamic gallery that displays various images from different l
 - Media Queries: Used for responsive design and user-friendly interfaces at different screen width (styled for mobile-view, tablet-view and desktop-view).
 
 - BEM (Block Element Modifier): Used to enhance code readability and maintainability by clearly defining the structure and relationships of elements.
+
+- DOM (Document Object Model): Used to enable dynamic updates to the webpage in response to user actions, creating and managing elements, and ensuring a responsive and interactive experience.
+
+JavaScript: Serves as the backbone of the interactivity in the project, managing user inputs, dynamically generating content, and ensuring a smooth and responsive user experience.
 
 ## Screenshots
 
--- scripts/index.js
+++ scripts/index.js
@@ -25,16 +25,64 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
-
+/* ----------------------------------------------------------------------------- */
+/*                               Elements                                        */
+/* ----------------------------------------------------------------------------- */
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseModal = document.querySelector("#modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".gallery__cards");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
+/* ----------------------------------------------------------------------------- */
+/*                               Functions                                       */
+/* ----------------------------------------------------------------------------- */
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  return cardElement;
+}
+
+/* ----------------------------------------------------------------------------- */
+/*                                  Handlers                                     */
+/* ----------------------------------------------------------------------------- */
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/* ----------------------------------------------------------------------------- */
+/*                               Event Listeners                                 */
+/* ----------------------------------------------------------------------------- */
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-profileCloseModal.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+profileCloseModal.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
 });
