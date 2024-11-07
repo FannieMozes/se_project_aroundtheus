a05cc622111840dcf6d376fbf649a915a8c17ee5
Fannie Mozes <fanniejmozes@gmail.com>
2024-11-07 18:47:56 +0200

    Project 5. Around the U.S. - added functionality to the card gallery - Created a modal to preview pictures, which can be opened and closed. Implemented the Like button functionality to toggle the heart color when clicked. Added a form to create new cards, allowing users to add a caption and image url. Implementation of a delete button to remove cards from the gallery. Additionally, implemented smooth opening and closing transitions for modals to improve the user experience.

--- scripts/index.js
+++ scripts/index.js
@@ -44,20 +44,56 @@ const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 const addNewCardModal = document.querySelector("#add-card-modal");
 const addNewCardButton = document.querySelector(".profile__add-button");
+
 const addNewCardForm = addNewCardModal.querySelector(".modal__form");
 const addNewCardCloseModal = addNewCardModal.querySelector(".modal__close");
+
 /* ----------------------------------------------------------------------------- */
 /*                               Functions                                       */
 /* ----------------------------------------------------------------------------- */
 
+function openPopup(modal) {
+  modal.style.display = "flex";
+  modal.classList.add("modal_opened");
+}
+
 function closePopup(modal) {
   modal.classList.remove("modal_opened");
+  setTimeout(() => {
+    modal.classList.add("hidden");
+  }, 500);
 }
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  const imageModal = document.querySelector("#preview-image-modal");
+  const modalImagePreview = imageModal.querySelector(".modal__image-preview");
+  const modalImageCaption = imageModal.querySelector(".modal__image-caption");
+  const closeButton = imageModal.querySelector("#modal-close-button");
+
+  cardImageEl.addEventListener("click", () => {
+    modalImagePreview.src = cardData.link;
+    modalImagePreview.alt = cardData.name;
+    modalImageCaption.textContent = cardData.name;
+    imageModal.classList.add("modal_opened");
+  });
+
+  closeButton.addEventListener("click", () => {
+    closePopup(imageModal);
+  });
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
 
   cardTitleEl.textContent = cardData.name;
   cardImageEl.src = cardData.link;
@@ -74,6 +110,23 @@ function handleProfileEditSubmit(e) {
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
   closePopup(profileEditModal);
+}
+
+function handleAddCardFormSubmit(e) {
+  e.preventDefault();
+  const cardTitleInput = document.querySelector("#card-title-input").value;
+  const imageLinkInput = document.querySelector("#image-link-input").value;
+
+  const newCardData = {
+    name: cardTitleInput,
+    link: imageLinkInput,
+  };
+
+  const newCardElement = getCardElement(newCardData);
+
+  cardListEl.prepend(newCardElement);
+
+  closePopup(addNewCardModal);
 }
 
 /* ----------------------------------------------------------------------------- */
@@ -98,6 +151,8 @@ addNewCardButton.addEventListener("click", () => {
 addNewCardCloseModal.addEventListener("click", () =>
   closePopup(addNewCardModal)
 );
+
+addNewCardForm.addEventListener("submit", handleAddCardFormSubmit);
 
 initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
